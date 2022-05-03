d3b7ccbedf3181567660eac12012c7049a9e014e
Adolfo Matias <petadolfo@gmail.com>
2022-05-03 13:06:09 -0300

    Etapa Final: Post e Get com flask

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
 FLASK_APP=do.py
+FLASK_ENV=development
--- do.py
+++ do.py
@@ -1,18 +1,39 @@
 from app import app
 import os
 
-from flask import render_template
+from flask import render_template, request,redirect,flash
 
 @app.route("/")
-@app.route("/index")
+@app.route("/index/")
 def novo():
     
-    dados = {"profissao": "Professora"}
-    return render_template("index.html", dados=dados)
+    dados = {"profissao": "Profissao"}
+    
+    return render_template("index.html",  dados=dados)
+   
 
 @app.route("/contato")
 def contato():
     return render_template("contato.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+# @app.route("/autenticar", methods=["GET"])
+# def autenticar():
+#     usuar= request.args.get("usuar")
+#     passo= request.args.get("passo")
+#     return f"{str(usuar)}, {str(passo)}"
+
+@app.route("/autenticar", methods=["POST"])
+def autenticar():
+    usuar= request.form.get("usuar")
+    passo= request.form.get("passo")
+    return f"{str(usuar)}, {str(passo)}"
+
+
+
 
     
 # def  add():
